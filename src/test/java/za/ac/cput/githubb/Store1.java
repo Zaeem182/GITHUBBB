4feb85ffa0d2ad7ddc7ca904129db40c555f68a2
Pixelaters <Zpetersen@Bainscorp.com>
2021-03-31 17:47:49 +0200

    added stuff

---
+++ src/test/java/za/ac/cput/githubb/Store1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.githubb;
+
+/**
+ *
+ * @author zpete
+ */
+public class Store1 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
